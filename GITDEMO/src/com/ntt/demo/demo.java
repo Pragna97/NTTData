543c95dc8a5a2fd4f5298f80d377eaa1c957739a
Pragna97 <msapragna@gmail.com>
2020-03-26 14:35:48 +0530

    A demo on GIT

---
+++ GITDEMO/src/com/ntt/demo/demo.java
@@ -0,0 +1,14 @@
+package com.ntt.demo;
+
+import java.util.Scanner;
+
+public class demo {
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter your name:");
+		String name=sc.next();
+		
+		System.out.println("WELCOME "+name.toUpperCase()+" to GIT TUTORRIALS!!");
+}
+}
